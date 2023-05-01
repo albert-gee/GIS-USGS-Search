1b7e84778079f6783a27b010814cc7a4ee5453a2
Albert Gee <albert.gee.ca@gmaill.com>
2023-04-30 22:50:22 -0700

    Move GISRecord class declaration to GISRecord.h from GISRecord.cpp

--- src/GISRecord.h
+++ src/GISRecord.h
@@ -1,10 +1,41 @@
-#ifndef FINAL_GISRECORD_H
-#define FINAL_GISRECORD_H
+#ifndef GISRECORD_H
+#define GISRECORD_H
 
+#include <string>
 
 class GISRecord {
+private:
+    int feature_id;
+    std::string feature_name;
+
+    std::string feature_class;
+
+    std::string state_alpha;
+    std::string state_numeric;
+    std::string county_name;
+    std::string county_numeric;
+
+    std::string primary_latitude_DMS;
+    std::string primary_longitude_DMS;
+    std::string primary_latitude_DEC;
+    std::string primary_longitude_DEC;
+
+    std::string source_latitude_DMS;
+    std::string source_longitude_DMS;
+    std::string source_latitude_DES;
+    std::string source_longitude_DES;
+
+    int elevation_meters;
+
+    int elevation_feet;
+
+    std::string map_name;
+
+    std::string date_created;
+
+    std::string date_edited;
 
 };
 
 
-#endif //FINAL_GISRECORD_H
+#endif //GISRECORD_H
