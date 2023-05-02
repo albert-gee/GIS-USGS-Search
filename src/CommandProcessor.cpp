b670b2f4edecbeb1a69da631a44d3ef1ad406013
Albert Gee <albert.gee.ca@gmaill.com>
2023-05-01 23:57:34 -0700

    Implement processCommand method in CommandProcessor.

--- src/CommandProcessor.cpp
+++ src/CommandProcessor.cpp
@@ -1,1 +1,11 @@
 #include "../include/CommandProcessor.h"
+
+#include <iostream>
+
+using namespace std;
+
+CommandProcessor::CommandProcessor() = default;
+
+void CommandProcessor::processCommand(const string &command) {
+    std::cout << "Processing command: " << command << std::endl;
+}
