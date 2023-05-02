a9f601a1a1888ea10fdac962c8c14c2194b8ac1e
Albert Gee <albert.gee.ca@gmaill.com>
2023-05-01 23:57:14 -0700

    Define CommandProcessor class - constructor and processCommand.

--- include/CommandProcessor.h
+++ include/CommandProcessor.h
@@ -1,10 +1,16 @@
-#ifndef FINAL_COMMANDPROCESSOR_H
-#define FINAL_COMMANDPROCESSOR_H
+#ifndef COMMANDPROCESSOR_H
+#define COMMANDPROCESSOR_H
 
+#include <string>
+#include <vector>
+#include <fstream>
+#include "Command.h"
 
 class CommandProcessor {
-
+public:
+    CommandProcessor();
+    static void processCommand(const std::string& command);
 };
 
 
-#endif //FINAL_COMMANDPROCESSOR_H
+#endif //COMMANDPROCESSOR_H
