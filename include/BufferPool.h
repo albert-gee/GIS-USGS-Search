e8f1a965bb530fcb0da41630bbbf7244286a1a41
tibbydude <micgou@outlook.com>
2023-05-17 19:58:32 -0700

    buffer implmentation

--- include/BufferPool.h
+++ include/BufferPool.h
@@ -1,10 +1,17 @@
 #ifndef FINAL_BUFFERPOOL_H
 #define FINAL_BUFFERPOOL_H
-
+#include "GISRecord.h"
+#include <list>
+using namespace std;
 
 class BufferPool {
-
+public:
+    GISRecord* searchForRecord(int featrureId);
+private:
+    const int maxSize = 15;
+    list<GISRecord> buffer;
 };
+
 
 
 #endif //FINAL_BUFFERPOOL_H
--- src/BufferPool.cpp
+++ src/BufferPool.cpp
@@ -1,1 +1,32 @@
 #include "../include/BufferPool.h"
+
+GISRecord* BufferPool::searchForRecord(int featureId)
+{
+    int bufferIndex = 0;
+    for (GISRecord gisRecord : buffer) {
+        if (gisRecord.getFeatureId() == featureId) {
+            if (bufferIndex > 0) {
+                buffer.push_front(gisRecord);
+                list<GISRecord>::iterator it = buffer.begin();
+                std::advance(it, ++bufferIndex);
+                buffer.erase(it);
+            }
+
+            return &gisRecord;
+        }
+        ++bufferIndex;
+    }
+
+
+
+    GISRecord* gis = NULL;
+    //Insert code to search database and create a GISrecord and assign to gis variable
+
+
+    //If buffer limit is hit, pop the back and insert new record
+    if (buffer.size() == maxSize) {
+        buffer.pop_back();
+        buffer.push_front(*gis);
+    }
+    return gis;
+}
