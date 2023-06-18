ec9d8fc2413f3ed7aba1348556e7e735bb1b6502
Albert <albert.gee.ca@gmail.com>
2023-06-17 17:22:39 -0700

    Add comments to CommandProcessor.cpp

--- src/CommandProcessor.cpp
+++ src/CommandProcessor.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <list>
+#include <utility>
 
 using namespace std;
 
@@ -10,22 +11,33 @@ enum Commands{
 };
 
 CommandProcessor::CommandProcessor(string databaseFileName, string logFileName){
-    CommandProcessor::databaseFileName = databaseFileName;
-    CommandProcessor::logFileName = logFileName;
+    CommandProcessor::databaseFileName = std::move(databaseFileName);
+    CommandProcessor::logFileName = std::move(logFileName);
     CommandProcessor::systemManager = SystemManager();
 }
 
-
+// Process a command from the command file.
+// Lines beginning with a semicolon and blank lines are ignored.
+// Other lines consist of tokens separated by single Tab characters. A line terminator follow the last token on a line.
 void CommandProcessor::processCommand(const string &command) {
+    // Special Characters
     char commentIndicator = ';';
     char delim1 = '\t';
     char delim2 = ' ';
     char end = '\0';
+
     list<string> args;
     string temp;
     int i = 0;
+
+    // Check if the command is not a comment
     if(command[0] != commentIndicator) {
+
+        // Parse the command character by character until the end of the command
         while (command[i] != end) {
+
+            // Check if the character is not a delimiter and add it to the temp string.
+            // If the character is a delimiter, add the temp string to the args list and clear the temp string.
             if (command[i] != delim1 && command[i] != delim2) {
                 temp += command[i];
             } else {
@@ -42,8 +54,11 @@ void CommandProcessor::processCommand(const string &command) {
             }
         }
 
+        // The first argument is the function name
         string function = args.front();
         args.pop_front();
+
+        // Execute the function
         if(function == "import"){
             string recordFileName = args.front();
             cout << "import " << recordFileName << endl;
