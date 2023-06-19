7c929d21efdf7c03c20959b4449b002cbff213ad
Albert <albert.gee.ca@gmail.com>
2023-06-18 18:03:18 -0700

    Change GIS: add constants, changes the process of initializing system components such as Buffer pool and System manager.

--- src/GIS.cpp
+++ src/GIS.cpp
@@ -2,6 +2,18 @@
 #include <fstream>
 #include "../include/CommandProcessor.h"
 
+namespace Constants {
+    // The maximum number of records that can be stored in a bucket.
+    // If a bucket is full, the corresponding quadrant must be split into four sub-quadrants with their own buckets.
+    const unsigned long BUCKET_CAPACITY{4};
+
+    // These specific values encompass the latitude range from approximately 24.396308 (southernmost point) to
+    // 49.384358 (northernmost point), and the longitude range from approximately -125.000000 (westernmost point) to
+    // -66.934570 (easternmost point) of the contiguous United States.
+    const Point NORTH_WEST_POINT(49.384358, -125.000000);    // Top-left corner of the QuadTree's region
+    const Point SOUTH_EAST_POINT(24.396308, -66.934570);     // Bottom-right corner of the QuadTree's region
+}
+
 // This function takes the names of three files from the command line, like this:
 // ./GIS <database file name> <command script file name> <log file name>
 // Example: ./cmake-build-debug/GIS db_file.txt data/script01.txt log_file.txt
@@ -15,9 +27,9 @@ int main(int argc, char *argv[]) {
     }
 
     // Get the names of the files from the command line
-    std::string databaseFileName = argv[1];
-    std::string commandScriptFileName = argv[2];
-    std::string logFileName = argv[3];
+    const std::string databaseFileName = argv[1];
+    const std::string commandScriptFileName = argv[2];
+    const std::string logFileName = argv[3];
 
     /// DATABASE
     // Delete the database file if it already exists
@@ -56,7 +68,18 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    CommandProcessor cmdProcessor = CommandProcessor(databaseFileName, logFileName);
+    // Create the system components: BufferPool, NameIndex, QuadTree
+    BufferPool bufferPool{};
+    NameIndex nameIndex{};
+    QuadTree coordinateIndex = QuadTree(Constants::NORTH_WEST_POINT, Constants::SOUTH_EAST_POINT,
+                                        Constants::BUCKET_CAPACITY);
+
+    // Create the SystemManager. The SystemManager uses system components to import, index, store, and retrieve data.
+    SystemManager systemManager = SystemManager(nameIndex, coordinateIndex, bufferPool, databaseFileName, logFileName);
+
+    // Create the CommandProcessor. The CommandProcessor uses the SystemManager to execute commands.
+    CommandProcessor cmdProcessor = CommandProcessor(systemManager);
+
     // read each line from the file and pass it to the command processor
     std::string line;
     while (std::getline(commandScriptFile, line)) {
