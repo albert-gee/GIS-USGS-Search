d011a0e31411689ae72c48e73fad7f77ff94010b
Albert <albert.gee.ca@gmail.com>
2023-06-18 17:57:33 -0700

    Change LineUtility.h: - Remove LineUtility.cpp since all the methods are static - Add extractParametersFromLine, extractLocationFromLine, extractFeatureNameAndStateFromLine, createGISRecordFromLine, createGISRecordFromLine methods.

--- include/LineUtility.h
+++ include/LineUtility.h
@@ -8,14 +8,107 @@
 #include <string>
 #include <list>
 #include <vector>
+#include "coordinateIndex/Point.h"
+#include "GISRecord.h"
+#include <sstream>
 
 using namespace std;
 
 class LineUtility {
 public:
-    static vector<string> * extractParametersFromLine(const string &line);
+    // Extract the parameters from a line
+    static vector<string> * extractParametersFromLine(const string &line, char delimiter) {
+        int index = 0;
+        auto *parameters = new vector<string>();
+        ostringstream os;
+        while (line[index] != '\0') {
+            if (line[index] != delimiter) {
+                os << line[index];
+            } else {
+                parameters->push_back(os.str());
+                os.str("");
+                os.clear();
+            }
+            ++index;
+        }
+        if(parameters->size() < 20){
+            parameters->push_back(os.str());
+        }
+        return parameters;
+    }
+
+
+    // Extract the location from a line
+    static Point extractLocationFromLine(string line, int longitudeColumn, int latitudeColumn, char delimiter) {
+        int index = 0;
+        string latitude;
+        string longitude;
+        int column = 0;
+        while(line[index] != '\0' && column <= longitudeColumn){
+            if(line[index] != delimiter){
+                if(column == latitudeColumn) {
+                    latitude += line[index];
+                } else if(column == latitudeColumn){
+                    longitude += line[index];
+                }
+            } else {
+                ++column;
+            }
+            ++index;
+        }
+        return {std::stod(latitude), std::stod(longitude)};
+    }
+
+    // Extract the feature name and state alpha from a line
+    static string extractFeatureNameAndStateFromLine(string line, int featureNameColumn, int stateAlphaColumn, char delimiter) {
+        int index = 0;
+        string featureName;
+        string stateAlpha;
+        int column = 0;
+        while(line[index] != '\0' && column <= stateAlphaColumn){
+            if(line[index] != delimiter){
+                if(column == featureNameColumn) {
+                    featureName += line[index];
+                } else if(column == stateAlphaColumn){
+                    stateAlpha += line[index];
+                }
+            } else {
+                ++column;
+            }
+            ++index;
+        }
+        return featureName + " " + stateAlpha;
+    }
+
+
+    static GISRecord * createGISRecordFromLine(const string& line, char delimiter) {
+
+        vector<string> *params = LineUtility::extractParametersFromLine(line, delimiter);
+
+        auto * gisRecord = new GISRecord(stoi((*params)[0]),
+                                              (*params)[1],
+                                              (*params)[2],
+                                              (*params)[3],
+                                              (*params)[4],
+                                              (*params)[5],
+                                              (*params)[6],
+                                              (*params)[7],
+                                              (*params)[8],
+                                              (*params)[9],
+                                              (*params)[10],
+                                              (*params)[11],
+                                              (*params)[12],
+                                              (*params)[13],
+                                              (*params)[14],
+                                              stoi((*params)[15]),
+                                              stoi((*params)[16]),
+                                              (*params)[17],
+                                              (*params)[18],
+                                              (*params)[19]);
+        return gisRecord;
+    }
+
 private:
-    static const char DELIMITER = '|';
 };
 
 
