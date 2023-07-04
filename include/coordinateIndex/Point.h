9a84b20be17dbdecee18ff0ca41cf3dc0c60a3b5
Albert <albert.gee.ca@gmail.com>
2023-07-04 14:16:29 -0700

    Add a new Point constructor which accepts string coordinates

--- include/coordinateIndex/Point.h
+++ include/coordinateIndex/Point.h
@@ -2,17 +2,24 @@
 #define GIS_POINT_H
 
 #include <iostream>
+#include <iomanip>
 
 // This struct describes a geographic coordinate.
 struct Point {
     double latitude;
     double longitude;
 
+    Point(const std::string& lat, const std::string& lng){
+        latitude = stod(lat);
+        longitude = stod(lng);
+    }
+
     Point(double lat, double lng) : latitude{lat}, longitude{lng} {}
 
     // Print the point
     void print() const {
-        std::cout << "(" << latitude << ", " << longitude << ")" << std::endl;
+        std::cout << std::fixed << std::setprecision(7);
+        std::cout << "(" << latitude << ", " << longitude << ")";
     }
 };
 
