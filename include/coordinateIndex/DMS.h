d12288094e1ac0ef084cf0b7fefea46a9490432b
Albert <albert.gee.ca@gmail.com>
2023-07-03 17:45:29 -0700

    Set DMS struct

--- include/coordinateIndex/DMS.h
+++ include/coordinateIndex/DMS.h
@@ -1,8 +1,21 @@
 #ifndef DMS_H
 #define DMS_H
 
+#include <iostream>
+
+// This struct describes a geographic coordinate in the DMS format.
 struct DMS {
-    //
+    double degrees;   // Degrees component
+    double minutes;   // Minutes component
+    double seconds;   // Seconds component
+    char direction; // Direction (N, S, E, W)
+
+    DMS(double degrees, double minutes, double seconds, char direction) : degrees{degrees}, minutes{minutes}, seconds{seconds}, direction{direction} {}
+
+    // Print the DMS coordinate
+    void print() const {
+        std::cout << degrees << "°" << minutes << "'" << seconds << "\"" << direction;
+    }
 };
 
 #endif //DMS_H
