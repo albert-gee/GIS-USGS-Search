4af5df42492f07f702c324679be6995996a22c45
Albert <albert.gee.ca@gmail.com>
2023-07-06 19:59:37 -0700

    Update the structure of the command processor.

--- src/commandProcessor/CommandProcessor.cpp
+++ src/commandProcessor/CommandProcessor.cpp
@@ -10,8 +10,11 @@ enum Commands {
     import = 0
 };
 
-CommandProcessor::CommandProcessor(SystemManager systemManager, const std::string& commandScriptFileLocation)
-        : systemManager(std::move(systemManager)) {
+CommandProcessor::CommandProcessor(SystemManager systemManager)
+        : systemManager(std::move(systemManager)) {}
+
+void CommandProcessor::processCommandsFromScriptFile(const std::string &commandScriptFileLocation) {
+    std::ifstream commandScriptFile;
 
     // Open the command script file
     commandScriptFile.open(commandScriptFileLocation, std::ios::in | std::ios::out | std::ios::app);
@@ -20,38 +23,30 @@ CommandProcessor::CommandProcessor(SystemManager systemManager, const std::strin
     if (!commandScriptFile.is_open()) {
         std::cerr << "Error: Failed to open command script file." << std::endl;
     }
-}
 
-void CommandProcessor::processCommandsFromScriptFile() {
     // read each line from the file and pass it to the command processor
     std::string line;
     while (std::getline(commandScriptFile, line)) {
-        processCommand(line);
+        processLine(line);
     }
+
+    commandScriptFile.close();
 }
 
-// Process a line from the line file.
-// Lines beginning with a semicolon and blank lines are ignored.
-// Other lines consist of tokens separated by single Tab characters. A line terminator follow the last token on a line.
-void CommandProcessor::processCommand(const string &line) {
-    // Special Characters
-    char commentIndicator = ';';
-    char delim1 = '\t';
-    //char delim2 = ' ';
-    char end = '\0';
-
+void CommandProcessor::processLine(const string &line) {
     list<string> args;
     string temp;
     int i = 0;
+
     // Check if the line is not a comment
-    if (line[0] != commentIndicator) {
+    if (line[0] != COMMENT_INDICATOR) {
 
         // Parse the line character by character until the end of the line
-        while (line[i] != end) {
+        while (line[i] != END_CHAR) {
 
             // Check if the character is not a delimiter and add it to the temp string.
             // If the character is a delimiter, add the temp string to the args list and clear the temp string.
-            if (line[i] != delim1) {
+            if (line[i] != DELIM1) {
                 temp += line[i];
             } else {
                 if (!temp.empty()) {
@@ -60,7 +55,7 @@ void CommandProcessor::processCommand(const string &line) {
                 }
             }
             i++;
-            if (line[i] == end) {
+            if (line[i] == END_CHAR) {
                 if (!temp.empty()) {
                     args.push_back(temp);
                 }
@@ -71,112 +66,142 @@ void CommandProcessor::processCommand(const string &line) {
         string function = args.front();
         args.pop_front();
 
-        // Execute the function
-        // Import: load records into the databaseService from external files
-
-        if (function != "world") {
-            ++commandsProcessed;
-        }
-        systemManager.logCommand(commandsProcessed, function, args, delim1);
-        //systemManager.logComment(string(90, '-'));
-
-        if (function == "world") {
-            // Specify boundaries of coordinate space
-            string westLong = args.front();
-            args.pop_front();
-            string eastLong = args.front();
-            args.pop_front();
-            string southLat = args.front();
-            args.pop_front();
-            string northLat = args.front();
-            args.pop_front();
-
-            // The coordinates are in DMS format, so create DMS objects
-            DMS westLongDMS(westLong);
-            DMS eastLongDMS(eastLong);
-            DMS southLatDMS(southLat);
-            DMS northLatDMS(northLat);
-
-            // Set the boundaries of the coordinate space
-            systemManager.setCoordinateIndexBoundaries(northLatDMS.toDecimal(), southLatDMS.toDecimal(),
-                                                       eastLongDMS.toDecimal(), westLongDMS.toDecimal());
-        } else if (function == "import") {
-
-            // The next argument is the name of the file containing the records
-            string recordsDataSetFileName = args.front();
-
-
-            // Import the records into the databaseService
-            systemManager.import(recordsDataSetFileName);
-        } else if (function == "what_is_at") {
-            // The next argument is the latitude of the location
-            string latitude = args.front();
-
-            //cout << "lat: " << latitude << endl;
-            args.pop_front();
-
-            // The next argument is the latitude of the location
-            string longitude = args.front();
-            //cout << "lon: " << longitude << endl;
-
-            //cout << latitude.length() << endl;
-
-            // Import the records into the databaseService
-
-            // Find GIS records by coordinates
-            auto records = systemManager.findGISRecordsByCoordinates(Point(DMS(latitude).toDecimal(), DMS(longitude).toDecimal()));
-            // LineUtility::convertDMStoDEC(latitude), LineUtility::convertDMStoDEC(longitude)
-
-
-        } else if (function == "what_is") {
-            whatIs(args);
-  /*          string featureName = args.front();
-            args.pop_front();
-            string stateAbrv = args.front();
-            systemManager.whatIs(featureName, stateAbrv);*/
-
-        } else if (function == "what_is_in") {
-            bool isFiltered = false;
-            bool isDetailed = false;
-            string filter = "";
-            if(args.front() == "-filter"){
-                isFiltered = true;
-                args.pop_front();
-                filter = args.front();
-                args.pop_front();
-            } else if(args.front() == "-long"){
-                isDetailed = true;
-                args.pop_front();
-            }
-
-            double latitude = LineUtility::convertDMStoDEC(args.front());
-            args.pop_front();
-            double longitude = LineUtility::convertDMStoDEC(args.front());
-            args.pop_front();
-            double halfHeight = stod(args.front()) / 3600;
-            args.pop_front();
-            double halfWidth = stod(args.front()) / 3600;
-            systemManager.whatIsIn(isFiltered, isDetailed, filter, latitude, longitude, halfHeight, halfWidth);
-
-        } else if (function == "debug") {
-            string debugTarget = args.front();
-            if (debugTarget == "hash") {
-                systemManager.debugHash();
-            } else if(debugTarget == "pool"){
-                systemManager.debugPool();
-            }
-
-        } else if (function == "quit") {
-
-        }
+        // Pass the function name and arguments to processCommand()
+        processCommand(function, args);
     } else {
         systemManager.logComment(line);
     }
 }
 
-void CommandProcessor::whatIs(list<string> args) {
-    string featureName = args.front();
-    args.pop_front();
-    string stateAbrv = args.front();
+void CommandProcessor::processCommand(const string &function, list<string> &args) {
+
+    // Log the command and arguments EXCEPT for the world command
+    if (function != "world") {
+        ++commandsProcessed;
+    }
+    systemManager.logCommand(commandsProcessed, function, args, DELIM1);
+    //systemManager.logComment(string(90, '-'));
+
+    // Call the appropriate function based on the function name
+    if (function == "world") {
+
+        string westLong = args.front();
+        args.pop_front();
+        string eastLong = args.front();
+        args.pop_front();
+        string southLat = args.front();
+        args.pop_front();
+        string northLat = args.front();
+        args.pop_front();
+
+        world(westLong, eastLong, southLat, northLat);
+
+    } else if (function == "import") {
+
+        string recordsDataSetFileName = args.front();
+        import(recordsDataSetFileName);
+
+    } else if (function == "what_is_at") {
+        string latitude = args.front();
+        args.pop_front();
+
+        string longitude = args.front();
+        args.pop_front();
+
+        whatIsAt(latitude, longitude);
+
+    } else if (function == "what_is") {
+
+        string featureName = args.front();
+        args.pop_front();
+        string stateAbrv = args.front();
+
+        whatIs(featureName, stateAbrv);
+
+    } else if (function == "what_is_in") {
+        bool isFiltered = false;
+        bool isDetailed = false;
+        string filter;
+        if (args.front() == "-filter") {
+            isFiltered = true;
+            args.pop_front();
+            filter = args.front();
+            args.pop_front();
+        } else if (args.front() == "-long") {
+            isDetailed = true;
+            args.pop_front();
+        }
+
+        string latitude = args.front();
+        args.pop_front();
+
+        string longitude = args.front();
+        args.pop_front();
+
+        string halfHeight = args.front();
+        args.pop_front();
+
+        string halfWidth = args.front();
+        args.pop_front();
+
+        whatIsIn(isFiltered, isDetailed, filter, latitude, longitude, halfHeight, halfWidth);
+
+    } else if (function == "debug") {
+        string debugTarget = args.front();
+        debug(debugTarget);
+
+    } else if (function == "quit") {
+        quit();
+    }
+}
+
+void CommandProcessor::world(string &westLong, string &eastLong, string &southLat, string &northLat) {
+
+    // The coordinates are in DMS format, so create DMS objects
+    DMS westLongDMS(westLong);
+    DMS eastLongDMS(eastLong);
+    DMS southLatDMS(southLat);
+    DMS northLatDMS(northLat);
+
+    // Set the boundaries of the coordinate space
+    systemManager.setCoordinateIndexBoundaries(northLatDMS.toDecimal(), southLatDMS.toDecimal(),
+                                               eastLongDMS.toDecimal(), westLongDMS.toDecimal());
+}
+
+void CommandProcessor::import(string &recordsDataSetFileName) {
+    systemManager.import(recordsDataSetFileName);
+}
+
+// ToDo: complete this implementation
+void CommandProcessor::whatIsAt(string &latitude, string &longitude) {
+    list<GISRecord> gisRecords = systemManager.findGISRecordsByCoordinates(
+            Point(DMS(latitude).toDecimal(), DMS(longitude).toDecimal()));
+    // LineUtility::convertDMStoDEC(latitude), LineUtility::convertDMStoDEC(longitude)
+}
+
+// ToDo: complete this implementation
+void CommandProcessor::whatIs(string &featureName, string &stateAbrv) {
     systemManager.whatIs(featureName, stateAbrv);
+}
+
+void CommandProcessor::whatIsIn(bool isFiltered, bool isDetailed, string &filter, string &latitude, string &longitude,
+                                string &halfHeight, string &halfWidth) {
+    double ltd = LineUtility::convertDMStoDEC(latitude);
+    double lng = LineUtility::convertDMStoDEC(longitude);
+    double hlfh = stod(halfHeight) / 3600;
+    double hlfw = stod(halfWidth) / 3600;
+
+    systemManager.whatIsIn(isFiltered, isDetailed, filter, ltd, lng, hlfh, hlfw);
+}
+
+void CommandProcessor::debug(string &debugTarget) {
+    if (debugTarget == "hash") {
+        systemManager.debugHash();
+    } else if (debugTarget == "pool") {
+        systemManager.debugPool();
+    }
+}
+
+// ToDo: complete this implementation
+void CommandProcessor::quit() {
 }
