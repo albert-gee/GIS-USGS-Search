9e17a1f1b88f86d63d9c9b10c7cd13d1f6810786
Albert <albert.gee.ca@gmail.com>
2023-07-08 16:44:49 -0700

    Remove unused methods from LineUtility and use GisRecord instead.

--- include/database/LineUtility.h
+++ include/database/LineUtility.h
@@ -16,135 +16,60 @@ using namespace std;
 
 class LineUtility {
 public:
-    // Extract the parameters from a line
-    static vector<string> * extractParametersFromLine(const string &line, char delimiter) {
+
+    // Extract the fields from a line and create a GISRecord object from them.
+    static GISRecord* extractGISRecordFromLine(const string& line, char delimiter) {
+
+        vector<string> *fields = LineUtility::extractFieldsFromLine(line, delimiter);
+
+        return new GISRecord(
+            stoi((*fields)[0]),
+            (*fields)[1],
+            (*fields)[2],
+            (*fields)[3],
+            (*fields)[4],
+            (*fields)[5],
+            (*fields)[6],
+            (*fields)[7],
+            (*fields)[8],
+            (*fields)[9],
+            (*fields)[10],
+            (*fields)[11],
+            (*fields)[12],
+            (*fields)[13],
+            (*fields)[14],
+            stoi((*fields)[15]),
+            stoi((*fields)[16]),
+            (*fields)[17],
+            (*fields)[18],
+            (*fields)[19]
+        );
+    }
+
+private:
+
+    // Extract the fields from a line, such as feature name, state alpha, etc.
+    static vector<string> * extractFieldsFromLine(const string &line, char delimiter) {
         int index = 0;
-        auto *parameters = new vector<string>();
+        auto *fields = new vector<string>();
+
         ostringstream os;
         while (line[index] != '\0') {
             if (line[index] != delimiter) {
                 os << line[index];
             } else {
-                parameters->push_back(os.str());
+                fields->push_back(os.str());
                 os.str("");
                 os.clear();
             }
             ++index;
         }
-        if(parameters->size() < 20){
-            parameters->push_back(os.str());
+        if(fields->size() < 20){
+            fields->push_back(os.str());
         }
-        return parameters;
+
+        return fields;
     }
-
-
-    // Extract the location from a line
-    static Point extractLocationFromLine(string line, int longitudeColumn, int latitudeColumn, char delimiter) {
-
-        //int index = 0;
-        string latitude = extractParamFromLine(line, latitudeColumn, delimiter);
-        string longitude = extractParamFromLine(line, longitudeColumn, delimiter);
-        //cout << latitude << " " << longitude <<endl;
-        //int column = 0;
-
-/*        while(line[index] != '\0' && column <= longitudeColumn){
-            if(line[index] != delimiter){
-                if(column == latitudeColumn) {
-                    latitude += line[index];
-                } else if(column == latitudeColumn){
-                    longitude += line[index];
-                }
-            } else {
-                ++column;
-            }
-            ++index;
-        }*/
-        return {latitude, longitude};
-    }
-
-    // Extract the feature name and state alpha from a line
-    static string extractFeatureNameAndStateFromLine(string line, int featureNameColumn, int stateAlphaColumn, char delimiter) {
-        int index = 0;
-        string featureName;
-        string stateAlpha;
-        int column = 0;
-        while(line[index] != '\0' && column <= stateAlphaColumn){
-            if(line[index] != delimiter){
-                if(column == featureNameColumn) {
-                    featureName += line[index];
-                } else if(column == stateAlphaColumn){
-                    stateAlpha += line[index];
-                }
-            } else {
-                ++column;
-            }
-            ++index;
-        }
-        return featureName + " " + stateAlpha;
-    }
-
-    static string extractFeatureName(string line, int featureNameColumn, char delimiter) {
-        int index = 0;
-        ostringstream os;
-        int column = 0;
-        while(line[index] != '\0' && column <= featureNameColumn){
-            if(line[index] != delimiter){
-                if(column == featureNameColumn) {
-                    os << line[index];
-                }
-            } else {
-                ++column;
-            }
-            ++index;
-        }
-        return os.str();
-    }
-
-    static string extractParamFromLine(const string line, const int paramColumn, const char delimiter) {
-        int index = 0;
-        ostringstream os;
-        int column = 0;
-        while(line[index] != '\0' && column <= paramColumn){
-            if(line[index] != delimiter){
-                if(column == paramColumn) {
-                    os << line[index];
-                }
-            } else {
-                ++column;
-            }
-            ++index;
-        }
-        return os.str();
-    }
-
-    static GISRecord * createGISRecordFromLine(const string& line, char delimiter) {
-
-        vector<string> *params = LineUtility::extractParametersFromLine(line, delimiter);
-
-        auto * gisRecord = new GISRecord(stoi((*params)[0]),
-                                              (*params)[1],
-                                              (*params)[2],
-                                              (*params)[3],
-                                              (*params)[4],
-                                              (*params)[5],
-                                              (*params)[6],
-                                              (*params)[7],
-                                              (*params)[8],
-                                              (*params)[9],
-                                              (*params)[10],
-                                              (*params)[11],
-                                              (*params)[12],
-                                              (*params)[13],
-                                              (*params)[14],
-                                              stoi((*params)[15]),
-                                              stoi((*params)[16]),
-                                              (*params)[17],
-                                              (*params)[18],
-                                              (*params)[19]);
-        return gisRecord;
-    }
-
-private:
 };
 
 
