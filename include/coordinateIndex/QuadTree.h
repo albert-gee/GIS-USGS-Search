10bc4ff9120c6e5650534c61cb70b6395ca9fbc7
Albert <albert.gee.ca@gmail.com>
2023-07-08 16:42:38 -0700

    Update insert method of QuadTree, it accepts string values

--- include/coordinateIndex/QuadTree.h
+++ include/coordinateIndex/QuadTree.h
@@ -14,8 +14,11 @@ public:
     // Set the boundaries of the QuadTree
     void setBoundingBox(Point northWestPoint, Point southEastPoint);
 
-    // Insert an offset of a GIS record into the QuadTree
-    void insert(Point point, int offsetOfGISRecord);
+    // Insert location and offset of a GIS record into the QuadTree
+    //
+    // The offset is used to retrieve the GIS record from the database
+    // The location is expressed as latitude and longitude in decimal degrees provided as strings
+    void insert(const std::string& latitude, const std::string& longitude, int offsetOfGISRecord);
 
     // Get the offsets of the GIS records in the QuadTree that are within the given bounding box
     std::vector<int> getOffsetsOfGISRecords(Point northWestPoint, Point southEastPoint);
